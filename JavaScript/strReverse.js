f1dc05245e7f122fab9bcf3b4aedbf1452b1907c
FarhanFardid <farhanfardid009@gmail.com>
2024-05-16 18:02:05 +0600

    practice problem 1

---
+++ JavaScript/strReverse.js
@@ -0,0 +1,13 @@
+// Javascript Problem solving
+// Reverse a string. Write a function that takes a string as input and returns the string reversed.
+
+function strReverse (str) {
+    let modifiedStr = str.split('');
+    console.log(modifiedStr);
+    let reverseString = modifiedStr.reverse().join('');
+    console.log(reverseString); 
+    return reverseString;
+}
+
+let reverseStr = strReverse('Hello, World');
+console.log(reverseStr);
