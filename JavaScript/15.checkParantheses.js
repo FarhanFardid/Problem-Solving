64d939407bc4fcce1748ac6cf1e532f741c40e41
FarhanFardid <farhanfardid009@gmail.com>
2024-05-18 19:45:54 +0600

    Paranthese Balanced Check

---
+++ JavaScript/15.checkParantheses.js
@@ -0,0 +1,34 @@
+// Check for balanced parentheses. Write a function to determine if a given string of parentheses is balanced.
+
+const checkParantheses = (str) =>{
+    let track= [];
+    const paranthesesObj ={
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    };
+    for(let val of str){
+        if (val === '(' || val ==='{' || val === '['){
+            track.push(val);
+        }
+        else if(val === ')' || val ==='}' || val === ']')
+        {
+            let top = track.pop();
+            console.log(top);
+            if(paranthesesObj[top] !== val ){
+                return "Imbalanced";
+            }
+        }
+   if(track.length === 0){
+    return "Balanced";
+   }
+    }
+
+    console.log(track);
+
+}
+
+const string ='([({[]})])';
+const result = checkParantheses(string);
+
+console.log("The parantheses are: ", result);
