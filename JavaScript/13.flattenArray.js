607c9652fdfc14f7ba398de8ca0e2bc5c4396ac7
FarhanFardid <farhanfardid009@gmail.com>
2024-05-18 17:59:30 +0600

    Flatten Array from Mixed Array

--- JavaScript/13.flattenArray.js
+++ JavaScript/13.flattenArray.js
@@ -1,7 +1,22 @@
 // Flatten an array. Write a function to flatten a nested array.
 
 const flatArray =(array)=>{
+let simpleArray=[];
+
+for (let val of array){
+    console.log(val);
+    if(!Array.isArray(val)){
+        simpleArray.push(val);
+    }
+    else{
+       simpleArray = simpleArray.concat(flatArray(val));
+    }
 
 }
+return simpleArray;
+console.log(simpleArray);
+}
 
-const mixedNum = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10, [11, 12, 13], 14 ], 15, 16 ];
+const mixedNum = [1, 2, 3, [9, 10, [11, 12, 13], 14 ], [4, 5, 6], [7, 8],  15, 16 ];
+const result = flatArray(mixedNum);
+console.log("The array after Flatten to simple format is: ", result);
