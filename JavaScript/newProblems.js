dd2e80d9f878decb62ccbe8e72843c63084f020c
FarhanFardid <farhanfardid009@gmail.com>
2024-05-20 17:32:33 +0600

    First practice Phase completed

--- JavaScript/newProblems.js
+++
@@ -1,5 +0,0 @@
-
-// Advanced: 
-
-
-// 18. Create a JavaScript class for a linked list with methods to insert a node at the beginning, end, or at a specific position, and to delete a node from a given position. 
