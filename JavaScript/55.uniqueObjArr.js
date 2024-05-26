2be92420d23bca8f34b89a3cb98a7dbf52273b32
FarhanFardid <farhanfardid009@gmail.com>
2024-05-26 17:34:11 +0600

    Finding Unique Object from an Array of objects

--- JavaScript/55.uniqueObjArr.js
+++ JavaScript/55.uniqueObjArr.js
@@ -2,13 +2,32 @@
 // I/P: [{name: "sai"},{name:"Nang"},{name: "sai"},{name:"Nang"},{name: "111111"}];
 // O/P: [{name: "sai"},{name:"Nang"}{name: "111111"}
 
+// process-1
+// const uniqueObj = (arr) => {
+//   return arr.reduce((unique, item) => {
+//     if (!unique.some((obj) => obj.name === item.name)) {
+//       unique.push(item);
+//     }
+//     return unique;
+//   }, []);
+
+// process-2
 const uniqueObj = (arr) => {
-  return arr.reduce((unique, item) => {
-    if (!unique.some((obj) => obj.name === item.name)) {
-      unique.push(item);
+  let unique = [];
+  for (let i of arr) {
+    let duplicate = false;
+    for (let item of unique) {
+      if (JSON.stringify(item) === JSON.stringify(i)) {
+        duplicate = true;
+        break;
+      }
     }
-    return unique;
-  }, []);
+    if (!duplicate) {
+      unique.push(i);
+    }
+  }
+  console.log(unique);
+  return unique;
 };
 
 const mixedObj = [
